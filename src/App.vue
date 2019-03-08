2058e687188c5c153cec1ea85014c52540ea7a48
Warat Wongmaneekit (Thang) <canopybanj@gmail.com>
2019-03-08 20:02:23 +0700

    Finish v-html

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <ul>
-      <li v-for="todo in todos" :key="todo.time">
-        {{todo.text}}
+      <li v-for="todo in todos" :key="todo.time" v-html="todo.text">
       </li>
     </ul>
   </div>
