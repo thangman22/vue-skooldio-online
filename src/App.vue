6de44d897caf8446f1bca0d22323ae3bbed0baef
Warat Wongmaneekit (Thang) <canopybanj@gmail.com>
2019-03-08 19:53:51 +0700

    Init project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      todos:[
+        {
+            "text": "Learn Vue.js",
+            "completed": true,
+            "time": 1526710200,
+            "id": 1526710200
+        },
+        {
+            "text": "Learn VueX",
+            "completed": false,
+            "time": 1526710254,
+            "id": 1526710254
+        },
+        {
+            "text": "<strong>Learn Vue Router</strong>",
+            "completed": false,
+            "time": 1526710407,
+            "id": 1526710407
+        }
+      ]
+    }
   }
 }
 </script>
